eb55b56bb86d08bd016dc1e14919cee6e8f731a1
Sophia Rowe <sophia.rowe17@gmail.com>
2021-11-17 15:35:55 -0500

    volcano data successfully opened

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ import folium
 map = folium.Map(location=(35.61,-82.44),zoom_start = 50)
 map.save('map1.html')
 
-
-# FRED!
+import csv
+fv = open("data/volcanoes.tsv", "r" )
+dialect = csv.Sniffer().sniff(fv.read(10000))
+fv.seek(0)
+d = csv.DictReader(fv, dialect=dialect)
+rows = []
+for line in d:
+    rows.append(line)
